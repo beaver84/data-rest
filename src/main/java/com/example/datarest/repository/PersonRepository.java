23c63f69e489fa1dba4bd0ce4541dd8a05a8fdf5
Kyungtae Bae <Kyungtae@marvrus.com>
2022-03-31 10:07:57 +0900

    Initial commit

---
+++ src/main/java/com/example/datarest/repository/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.example.datarest.repository;
+
+import com.example.datarest.domain.Person;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+}
